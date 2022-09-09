7ea5542f443e5bb60849f23123826646514683f9
Sara Vieira <hey@iamsaravieira.com>
2022-09-09 23:58:33 +0300

    clean code

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,54 @@
-use anyhow::Result;
-use std::io::Write;
+use comfy_table::Table;
+use serde::{Deserialize, Serialize};
 
-pub const ERROR_PATTERN_EMPTY: &str = "Please pass a pattern";
-pub const ERROR_NO_RESULTS: &str = "No results found";
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Game {
+    id: i32,
+    igdb_id: i32,
+    first_release_date: i32,
+    total_rating: Option<f32>,
+    name: String,
+    slug: String,
+    console: String,
+}
 
-pub fn find_matches(pattern: &str, text: &str, mut writer: impl Write) -> Result<()> {
-    let mut results = Vec::new();
-    for line in text.lines() {
-        if line.to_lowercase().contains(&pattern.to_lowercase()) {
-            results.push(line)
-        }
+#[tokio::main]
+pub async fn get_searched_game(query: &str) -> Result<(), anyhow::Error> {
+    let mut table = Table::new();
+    table.set_header(vec!["Name", "Console", "Rating", "Link"]);
+    let result = reqwest::get(
+        "https://letsplayretro.games/api/search?query=".to_owned() + &query.to_owned(),
+    )
+    .await?
+    .json::<Vec<Game>>()
+    .await?;
+
+    for game in result {
+        let url = "https://letsplayretro.games/".to_owned()
+            + &game.console.to_owned()
+            + "/"
+            + &game.slug.to_owned();
+
+        let rating = match game.total_rating {
+            Some(i) => format!("{:.2}", i.to_string()),
+            _ => "Unknown".to_string(),
+        };
+        table.add_row(vec![&game.name, &game.console, &rating.to_string(), &url]);
     }
-
-    if results.len() > 0 {
-        for result in results {
-            writeln!(writer, "{}", result)?;
-        }
-    } else {
-        writeln!(writer, "{}", ERROR_NO_RESULTS)?;
-    }
-
+    println!("{table}");
     Ok(())
 }
 
-#[test]
-
-fn find_a_match() {
-    let mut result = Vec::new();
-
-    find_matches("sup", "sup\nomg\nsupagain", &mut result);
-    assert_eq!(result, b"sup\nsupagain\n")
+#[tokio::main]
+pub async fn get_random_game(console: &str) -> Result<(), anyhow::Error> {
+    let url =
+        "https://letsplayretro.games/api/".to_owned() + &console.to_owned() + &"/random".to_owned();
+    let game = reqwest::get(url).await?.json::<Game>().await?;
+    let url = "https://letsplayretro.games/".to_owned()
+        + &game.console.to_owned()
+        + "/"
+        + &game.slug.to_owned();
+    println!("{:#?}", game.name);
+    println!("{:#?}", url);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
 use clap::Parser;
-use comfy_table::Table;
-use serde::{Deserialize, Serialize};
+
+use letsplayretrogames::{get_random_game, get_searched_game};
 
 // enum CONSOLES {
 //     Nes,
@@ -15,16 +14,6 @@ use serde::{Deserialize, Serialize};
 //     Ms,
 //     Pce,
 // }
-#[derive(Debug, Deserialize, Serialize)]
-struct Game {
-    id: i32,
-    igdb_id: i32,
-    first_release_date: i32,
-    total_rating: Option<f32>,
-    name: String,
-    slug: String,
-    console: String,
-}
 
 #[derive(Parser)]
 struct SearchParams {
@@ -45,47 +34,6 @@ enum Command {
 struct Cli {
     #[clap(subcommand)]
     command: Command,
-}
-
-#[tokio::main]
-async fn get_searched_game(query: &str) -> Result<(), anyhow::Error> {
-    let mut table = Table::new();
-    table.set_header(vec!["Name", "Console", "Rating", "Link"]);
-    let result = reqwest::get(
-        "https://letsplayretro.games/api/search?query=".to_owned() + &query.to_owned(),
-    )
-    .await?
-    .json::<Vec<Game>>()
-    .await?;
-
-    for game in result {
-        let url = "https://letsplayretro.games/".to_owned()
-            + &game.console.to_owned()
-            + "/"
-            + &game.slug.to_owned();
-
-        let rating = match game.total_rating {
-            Some(i) => format!("{:.2}", i.to_string()),
-            _ => "Unknown".to_string(),
-        };
-        table.add_row(vec![&game.name, &game.console, &rating.to_string(), &url]);
-    }
-    println!("{table}");
-    Ok(())
-}
-
-#[tokio::main]
-async fn get_random_game(console: &str) -> Result<(), anyhow::Error> {
-    let url =
-        "https://letsplayretro.games/api/".to_owned() + &console.to_owned() + &"/random".to_owned();
-    let game = reqwest::get(url).await?.json::<Game>().await?;
-    let url = "https://letsplayretro.games/".to_owned()
-        + &game.console.to_owned()
-        + "/"
-        + &game.slug.to_owned();
-    println!("{:#?}", game.name);
-    println!("{:#?}", url);
-    Ok(())
 }
 
 fn main() {
